5a5c59a185125f9fdebb7691f8f19a3680b9d683
CindAlfi99 <acindi38@gmail.com>
2021-05-31 22:50:06 +0700

    update

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", function(event) {
+	event.waitUntil(
+		caches.open("pwa").then(function(cache) {
+			return cache.addAll([
+				"/",
+				"/asset/css/notif.css",
+				"/asset/css/bootstrap.min.css",
+				"/asset/js/sb-admin-2.js",
+				"/asset/js/order.js",
+				"/script.js",
+			]);
+		})
+	);
+});
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(
+		caches.open("pwa").then(function(cache) {
+			return cache.match(event.request).then(function(response) {
+				cache.addAll([event.request.url]);
+
+				if(response) {
+					return response;
+				}
+
+				return fetch(event.request);
+			});
+		})
+	);
+});
